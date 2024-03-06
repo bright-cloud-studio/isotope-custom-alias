628e06da08da61b661cd7f6bd38e83259522eb37
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 12:36:28 -0500

    Create IsotopeCustomAlias.php

---
+++ src/Resources/contao/library/Bcs/Backend/IsotopeCustomAlias.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Bright Cloud Studio's Isotope Export Orders
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
+namespace Bcs\Backend;
+
+use Isotope\Model\Payment;
+use Isotope\Model\ProductCollection\Order;
+use Isotope\Model\Shipping;
+
+class IsotopeCustomAlias extends Backend
+{
+  function doThing()
+  {
+    echo "BING";
+  }
+}
