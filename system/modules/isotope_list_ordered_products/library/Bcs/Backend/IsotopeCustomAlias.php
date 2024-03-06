991cdda7f28465f99e4458b40d2056e1377c39e4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 13:04:50 -0500

    Create IsotopeCustomAlias.php

---
+++ system/modules/isotope_list_ordered_products/library/Bcs/Backend/IsotopeCustomAlias.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+namespace Bcs\Backend;
+
+use Haste\Haste;
+use Isotope\Isotope;
+use Isotope\Interfaces\IsotopeProduct;
+use Isotope\Interfaces\IsotopeProductCollection;
+use Isotope\Model\Product;
+use Isotope\Model\ProductCollection\Order;
+
+
+class IsotopeCustomAlias extends \Isotope\Backend\Product\Alias
+{
+
+}
