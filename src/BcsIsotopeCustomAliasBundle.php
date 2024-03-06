433fa2212ec7d4f3b98283476a3ee0dd102a3229
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 12:28:01 -0500

    Create BcsIsotopeCustomAliasBundle.php

---
+++ src/BcsIsotopeCustomAliasBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Grouping
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/isotope-custom-alias
+ */
+
+namespace Bcs\IsotopeCustomAliasBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsIsotopeCustomAliasBundle extends Bundle
+{
+}
