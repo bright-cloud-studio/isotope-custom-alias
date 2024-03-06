ba9dbe428022aefdf1773d1582ddc894c505f703
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 13:32:51 -0500

    Update IsotopeCustomAlias.php

--- system/modules/isotope_custom_alias/library/Bcs/Backend/IsotopeCustomAlias.php
+++ system/modules/isotope_custom_alias/library/Bcs/Backend/IsotopeCustomAlias.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Bright Cloud Studio's Isotope List Ordered Products
+ * Bright Cloud Studio's Isotope Custom Alias
  *
- * Copyright (C) 2023 Bright Cloud Studio
+ * Copyright (C) 2024 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @package    bright-cloud-studio/isotope-custom-alias
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
@@ -14,6 +14,7 @@
 namespace Bcs\Backend;
 
 use Haste\Haste;
+use Haste\Input\Input;
 use Isotope\Isotope;
 use Isotope\Interfaces\IsotopeProduct;
 use Isotope\Interfaces\IsotopeProductCollection;
@@ -23,13 +24,51 @@ use Isotope\Model\ProductCollection\Order;
 
 class IsotopeCustomAlias extends \Isotope\Backend\Product\Alias
 {
-    public function saveCustom($varValue, DataContainer $dc)
+    public function saveCustom($varValue, \DataContainer $dc)
     {
         $autoAlias = false;
         $varValue  = (string) $varValue;
 
-        $varValue = "BING BONG NOISE";
+        // Generate alias if there is none
+        if ('' === $varValue) {
+            $autoAlias = true;
+            $act       = Input::get('act');
 
+            if ('edit' === $act || 'overrideAll' === $act) {
+                $varValue = (string) (Input::post('name') ?: Input::post('sku'));
+            } elseif ('editAll' === $act) {
+                $varValue = (string) (Input::post('name_'.$dc->id) ?: Input::post('sku_'.$dc->id));
+            }
+        }
+
+        if ('' === $varValue) {
+            $varValue = (string) ($dc->activeRecord->name ?: $dc->activeRecord->sku);
+        }
+
+        if ('' === $varValue) {
+            $varValue = $dc->id;
+        }
+
+        $varValue = \StringUtil::standardize(strip_tags($varValue));
+
+        $objAlias = \Database::getInstance()
+            ->prepare('SELECT id FROM tl_iso_product WHERE id=? OR alias=?')
+            ->execute($dc->id, $varValue)
+        ;
+
+        // Check whether the product alias exists
+        if ($objAlias->numRows > 1) {
+            if (!$autoAlias) {
+                throw new \OverflowException(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $varValue));
+            }
+
+            $varValue .= '.' . $dc->id;
+        }
+
+        // Get the first three characters, if it is 'id-' then return a stripped down version of our alias
+        $check = $varValue[0] . $varValue[1] . $varValue[2];
+        if($check == 'id-')
+            return substr($varValue, 3);
         return $varValue;
     }
 }
