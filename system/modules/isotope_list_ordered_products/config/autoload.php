917581da959da7214977024459b270dcb593b70c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 13:02:12 -0500

    Create autoload.php

---
+++ system/modules/isotope_list_ordered_products/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+// Register Classes
+ClassLoader::addClasses(array
+(
+    'Bcs\Backend\IsotopeCustomAlias'         => 'system/modules/isotope_custom_alias/library/Bcs/Backend/IsotopeCustomAlias.php',
+));
