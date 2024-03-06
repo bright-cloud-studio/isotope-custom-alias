456f0b84edbfbde252ea04815a6e01d1cbde7041
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 12:30:44 -0500

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Grouping
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/isotope-custom-alias
+ */
+  
+    //$GLOBALS['TL_WRAPPERS']['stop'][]  = 'grouping_stop';
+
+?>
