3f6a1991fe38a0643e346356ee6b9cd2cea252ce
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-06 12:52:36 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,7 +9,7 @@
  */
   
 
-$GLOBALS['TL_DCA']['tl_iso_product']['fields']['alias']['save_callback'] = array('Bcs\Backend\IsotopeCustomAlias', 'saveCustom');
+$GLOBALS['TL_DCA']['tl_iso_product']['fields']['alias']['save_callback'][''] = array('Bcs\Backend\IsotopeCustomAlias', 'saveCustom');
 
 
 
